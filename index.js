05253466fcc9a0b462c47831085585fb74a5a019
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-05 17:01:24 -0800

    completed

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 // Code your solutions in this file
-/*
-for (let age = 30; age < 40; age++) {
+
+/* for (let age = 30; age < 40; age++) {
     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+ } */
+
+
+ const gifts = ["teddy bear", "drone", "doll"];
+
+ function wrapGifts(gifts) {
+   for (let i = 0; i < gifts.length; i++) {
+     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+   }
+ 
+   return gifts;
  }
-*/
+ 
+ wrapGifts(gifts);
 
-const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
-    return gifts;
-}
+const newArry = []
 
-wrapGifts(gifts);
-
-const name = ["Poco", "Brook", "Yuzu"];
-const event = "suprise";
-const newArray = [];
 
 function writeCards(name, event) {
-    for (let x = 0; x < name.length; x++) {
-        newArray[x] = `Thank you, ${name[x]}, for the wonderful ${event} gift!`
-    debugger;
+    for (let i = 0; i < name.length; i++) {
+        newArry[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
     }
-    return newArray
+    return newArry;
 }
 
-writeCards(["Poco", "Brook", "Yuzu"], "suprise");
-
-function countDown() {
-let x = 10
-    while (x >= 0) {
-        console.log(x--);
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
     }
 }
-    countDown(10)
+
+countDown(10)
